45bb407cc68c35f214fcdf6a63ae0d41072c77a0
selfhoster1312 <selfhoster1312@kl.netlib.re>
2024-05-24 21:15:12 +0200

    Add documentation and example for helpers::string::capitalize

--- src/helpers/string.rs
+++ src/helpers/string.rs
@@ -1,3 +1,16 @@
+//! [`String`] and [`&str`]-related helpers.
+//! 
+//! Not a lot here yet...
+
+/// Capitalize the first letter of the given stringy value, returning a new string.
+///
+/// Example:
+///
+/// ```rust
+/// // Outputs: Hello, world!
+/// println("{}", capitalize("hello, world!"));
+/// # assert_eq!("Hello, world!".to_string(), capitalize("hello, world!"));
+/// ```
 pub fn capitalize<T: AsRef<str>>(string: T) -> String {
     let mut chars = string.as_ref().chars();
     match chars.next() {
